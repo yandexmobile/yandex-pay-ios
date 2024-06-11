79a2c6e61b2d28efeb889c9ce96189d8f2c5860b
Matvey Kravtsov <kravtsovguy@gmail.com>
2024-06-11 15:12:51 +0300

    YandexPaySDK 1.8.0

--- Package.swift
+++ Package.swift
@@ -7,12 +7,23 @@ let package = Package(
     .iOS(.v13),
   ],
   products: [
-    .library(name: "YandexPaySDK", targets: ["YandexPaySDK"]),
+    .library(name: "YandexPaySDK", targets: ["YandexPaySDK", "Varioqub", "VQSwiftProtobuf"]),
   ],
   targets: [
     .binaryTarget(
       name: "YandexPaySDK",
-      url: "https://github.com/yandexmobile/yandex-pay-ios/releases/download/v1.7.0/YandexPaySDK.xcframework.zip",
-      checksum: "36f7fb91e688a1fd5127707b7d4e4580033cbd063ea07c28f048b055e5591079")
+      url: "https://github.com/yandexmobile/yandex-pay-ios/releases/download/v1.8.0/YandexPaySDK.xcframework.zip",
+      checksum: "d2f224ea0f6dfb6dce043ca7bfdaa1fb04b5fd8676d34ff85cd1bb7b5f2959ab"
+    ),
+    .binaryTarget(
+      name: "Varioqub",
+      url: "https://github.com/yandexmobile/yandex-pay-ios/releases/download/v1.8.0/Varioqub.xcframework.zip",
+      checksum: "cef9d1325350d4e643063b247bb3afd82d8f07bf4832f1ac4521344f29bdda61"
+    ),
+    .binaryTarget(
+      name: "VQSwiftProtobuf",
+      url: "https://github.com/yandexmobile/yandex-pay-ios/releases/download/v1.8.0/VQSwiftProtobuf.xcframework.zip",
+      checksum: "019fe3ebe1c9b0c95afb1f152ae02eb11d9e902790d85e1fbadc4d3c7ab9bdcc"
+    )
   ]
 )
