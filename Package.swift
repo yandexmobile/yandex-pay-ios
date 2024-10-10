5d05d069535919f20aacc748f0e1b81230356021
Matvey Kravtsov <kravtsovguy@gmail.com>
2024-10-10 15:30:43 +0300

    move YandexPaySDKSPMDemoAppApp.swift to local spm with static sdk

--- Package.swift
+++ Package.swift
@@ -1,17 +1,64 @@
 // swift-tools-version:5.9
 import PackageDescription
 
+let yandexPay_Resources_Path = "Frameworks/Static/YandexPaySDKResources.bundle"
+let yandexPay_Static_Path = "Frameworks/Static/YandexPaySDK.xcframework"
+let yandexPay_Dynamic_Path = "Frameworks/Dynamic/YandexPaySDK.xcframework"
+let varioqub_Path = "Frameworks/Varioqub.xcframework"
+let vqSwiftProtobuf_Path = "Frameworks/VQSwiftProtobuf.xcframework"
+
 let package = Package(
   name: "YandexPaySDK",
   platforms: [
-    .iOS(.v13),
+    .iOS(.v14),
   ],
   products: [
-    .library(name: "YandexPaySDK", targets: ["YandexPaySDK", "Varioqub", "VQSwiftProtobuf"]),
+    .library(name: "YandexPaySDK", targets: [
+      "YandexPaySDK_Facade",
+      "Varioqub",
+      "VQSwiftProtobuf"
+    ]),
+    .library(name: "YandexPaySDK_Dynamic", targets: [
+      "YandexPaySDK_Dynamic",
+      "Varioqub",
+      "VQSwiftProtobuf"
+    ])
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/appmetrica/appmetrica-sdk-ios",
+      .upToNextMajor(from: "5.8.1")
+    ),
+    .package(
+      url: "https://github.com/yandexmobile/yandex-login-sdk-ios",
+      .upToNextMajor(from: "3.0.2")
+    )
   ],
   targets: [
-    .binaryTarget(name: "YandexPaySDK", path: "Frameworks/YandexPaySDK.xcframework"),
-    .binaryTarget(name: "Varioqub", path: "Frameworks/Varioqub.xcframework"),
-    .binaryTarget(name: "VQSwiftProtobuf", path: "Frameworks/VQSwiftProtobuf.xcframework")
+    .binaryTarget(name: "YandexPaySDK_Static", path: "YandexPaySDK/\(yandexPay_Static_Path)"),
+    .binaryTarget(name: "YandexPaySDK_Dynamic", path: "YandexPaySDK/\(yandexPay_Dynamic_Path)"),
+    .binaryTarget(name: "Varioqub", path: "YandexPaySDK/\(varioqub_Path)"),
+    .binaryTarget(name: "VQSwiftProtobuf", path: "YandexPaySDK/\(vqSwiftProtobuf_Path)"),
+    .target(
+      name: "YandexPaySDK_Facade",
+      dependencies: [
+        .target(name: "YandexPaySDK_Static"),
+        .product(name: "AppMetricaCore", package: "appmetrica-sdk-ios"),
+        .product(name: "YandexLoginSDK", package: "yandex-login-sdk-ios"),
+      ],
+      path: "YandexPaySDK",
+      exclude: [
+        yandexPay_Static_Path,
+        yandexPay_Dynamic_Path,
+        varioqub_Path,
+        vqSwiftProtobuf_Path
+      ],
+      sources: [
+        "Dummy.swift"
+      ],
+      resources: [
+        .copy(yandexPay_Resources_Path)
+      ]
+    )
   ]
 )
---
+++ YandexPaySDKSPMDemoApp/YandexPaySDKSPMDemoApp/YandexPaySDKSPMDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexPaySDKSPMDemoAppApp.swift
+//  YandexPaySDKSPMDemoApp
+//
+//  Created by Matvey Kravtsov on 22.08.24.
+//
+
+import SwiftUI
+
+@main
+struct YandexPaySDKSPMDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
